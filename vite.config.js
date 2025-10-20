e60706bde1f44a64cabdbc3edde75d8f80e7e954
terezkaa74 <tereza.gorgolova@gmail.com>
2025-10-20 10:57:11 +0200

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/webKnowSpace/', 
+  base: process.env.NODE_ENV === 'production' ? '/webKnowSpace/' : '/',
   server: {
     port: 3000
   }
