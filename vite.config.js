62aef66e3cd84742a40d9f9b89c4d3f54b09a3e5
terezkaa74 <tereza.gorgolova@gmail.com>
2025-10-20 11:00:39 +0200

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/webKnowSpace/' : '/',
-  server: {
-    port: 3000
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    base: process.env.NODE_ENV === 'production' ? '/webKnowSpace/' : '/',
+    server: {
+      port: 3000
+    },
+    define: {
+      'import.meta.env.VITE_SUPABASE_URL': JSON.stringify(env.VITE_SUPABASE_URL),
+      'import.meta.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(env.VITE_SUPABASE_ANON_KEY)
+    }
   }
 })
 
